0138dd0faaf175e98559d20945e8a0e0c691a09e
David Castejon <ombengue@hotmail.com>
2023-02-15 08:59:01 +0100

    Finish visitor questionnaire component and add transition to main screen

--- src/App.js
+++ src/App.js
@@ -5,6 +5,9 @@ import './App.scss';
 import logo from './images/zombiecat-trans-logo.png';
 import logoHover from './images/zombiecat-hover-logo.png';
 
+import submitBtn from './images/icos/cat-paw-hollow-trans.png';
+import submitBtnPressed from './images/icos/cat-paw-full-trans.png';
+
 import WelcomeSeal from './components/WelcomeSeal';
 import VisitorQuestionnaire from './components/VisitorQuestionnaire';
 import Header from './components/Header';
@@ -22,23 +25,34 @@ function App() {
   }
 
   const [hasVisitorInfo, setHasVisitorInfo] = useState(false);
-  
+  const [visitorInfo, setVisitorInfo] = useState({name: "", org: ""});
+
+  console.log(visitorInfo);
+
   return (
     <main className="App">
-      <WelcomeSeal 
-        logoImages={[logo, logoHover]} 
-        logoAlt={"Cartoon-like logo depicting a cat"} 
-        tradeName={"ZombieCat"}
-        role={"Full Stack Developer"}
-        actionTexts={["Activate cat-enhanced view", "Go see (zombie)cat stuff", "Choose your cat dialect"]}
-        viewport={viewport}
-        initSealIsShrunk={initSealIsShrunk}
-        handleSealState={handleSealState}
-      />
+      {
+        !hasVisitorInfo &&
+        <WelcomeSeal 
+          logoImages={[logo, logoHover]} 
+          logoAlt={"Cartoon-like logo depicting a cat"} 
+          tradeName={"ZombieCat"}
+          role={"Full Stack Developer"}
+          actionTexts={["Activate cat-enhanced view", "Go see (zombie)cat stuff", "Choose your cat dialect"]}
+          viewport={viewport}
+          initSealIsShrunk={initSealIsShrunk}
+          handleSealState={handleSealState}
+        />
+      }
       {
         initSealIsShrunk &&
         !hasVisitorInfo &&
-          <VisitorQuestionnaire />
+        <VisitorQuestionnaire 
+          btnSubmitIcons={[submitBtn, submitBtnPressed]} 
+          submitBtnAlt={"Cat paw"}
+          setHasVisitorInfo={setHasVisitorInfo}
+          setVisitorInfo={setVisitorInfo}
+        />
       }
       
       {
--- src/components/VisitorQuestionnaire.js
+++ src/components/VisitorQuestionnaire.js
@@ -1,6 +1,8 @@
-import React, {useState} from 'react';
+import React, {useState, useEffect} from 'react';
 
-function VisitorQuestionnaire() {
+function VisitorQuestionnaire(props) {
+    const [questionnairePosition, setQuestionnairePosition] = useState("");
+    
     const [name, setName] = useState("");
     const [org, setOrg] = useState("");
 
@@ -11,27 +13,46 @@ function VisitorQuestionnaire() {
         } else {
             setOrg(value);
         }
+
+        if (name && org) {
+            setBtnStatus("btn-enabled");
+        }
     }
 
+    const [btnStatus, setBtnStatus] = useState("");
     const handleSubmit = (event) => {
         event.preventDefault();
+        setBtnStatus("btn-pressed");
         console.log("Info submitted: ", name, " ", org)
+        setQuestionnairePosition("");
+        setTimeout(() => {
+            props.setHasVisitorInfo(true)
+            props.setVisitorInfo({name: name, org: org})
+        }, 4000);
     }
+
+    useEffect(() => {
+        setQuestionnairePosition("questionnaire--centered");
+    }, [])
     
     return (
-        <div className="questionnaire u-viewport-center">
+        <div className={"questionnaire" + " " + questionnairePosition + " " + "u-viewport-center" + " " + "u-blockElm-h-center"}>
+            <h3 className="questionnaire__hint">Kindly identify yourself so you can be addressed properly</h3>
             <form onSubmit={(e) => handleSubmit(e)}>
                 <div className="questionnaire__inputWrapper">
-                    <label className="questionnaire__inputLabel">Your name:</label>
+                    <label htmlFor="name-input" className="questionnaire__inputLabel">Your name:</label>
                     <input className="questionnaire__input" id="name-input" name="name-input" value={name} onChange={(e) => handleInput(e)} />
                 </div>
                 
                 <div className="questionnaire__inputWrapper">
-                    <label className="questionnaire__inputLabel">Your organization:</label>
+                    <label htmlFor="org-input" className="questionnaire__inputLabel">Your organization:</label>
                     <input className="questionnaire__input" id="org-input" name="org-input" value={org} onChange={(e) => handleInput(e)} />
                 </div>
 
-                <button>Submit</button>
+                <button className="questionnaire__submitBtn">
+                    {(!name || !org) && <img src={props.btnSubmitIcons[0]} alt={props.submitBtnAlt} />}
+                    {(name && org) && <img className={btnStatus} src={props.btnSubmitIcons[1]} alt={props.submitBtnAlt} />}
+                </button>
             </form>
         </div>
     )
