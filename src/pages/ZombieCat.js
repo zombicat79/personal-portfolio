7caad46804dd393367d7d498885dc616474fc206
David Castejon <ombengue@hotmail.com>
2023-04-25 19:37:52 +0200

    Complete ZombieCat page with final details

--- src/pages/ZombieCat.js
+++ src/pages/ZombieCat.js
@@ -5,6 +5,8 @@ import { LangContext } from "./../App";
 
 import zombiecatTexts from './../texts/ZombieCat_texts';
 
+import instagramLogo from './../images/logos/instagram-logo.png';
+
 function ZombieCat(props) {
     const language = useContext(LangContext);
 
@@ -18,13 +20,20 @@ function ZombieCat(props) {
         }, 6000)
     }, [])
 
-    const [content, setContent] = useState({cover: "u-visible", page1: "u-invisible"})
+    const [content, setContent] = useState({cover: "u-visible", page1: "u-invisible", page2: "u-invisible"})
     const handleContent = (contentType) => {
         if (contentType === "text") {
-            setContent({cover: "u-invisible", page1: "u-visible"});
+            setContent({cover: "u-invisible", page1: "u-visible", page2: "u-visible"});
         } else {
-            setContent({cover: "u-visible", page1: "u-invisible"});
+            setContent({cover: "u-visible", page1: "u-invisible", page2: "u-invisible"});
         }
+    }
+
+    const [easterEgg, setEasterEgg] = useState(false)
+    const handleEasterEgg = () => {
+        setEasterEgg((prevState) => {
+            return !prevState;
+        })
     }
 
     // Adjust component styling on render (correct styles from WelcomeSeal component)
@@ -41,30 +50,58 @@ function ZombieCat(props) {
         })
     })
 
+    console.log(easterEgg)
+
     return (
         <section className="page-body">
             <LaserBox handleContent={handleContent}>
-                <div className={`laserbox__content-wrapper laserbox__content-wrapper--${contentState}`}>
+                <div className={`laserbox__content-wrapper laserbox__content-wrapper--zombiecat laserbox__content-wrapper--${contentState}`}>
                     {content.cover === "u-visible" &&
                         <div className={`laserbox__content-section laserbox__content-section--cat ${content.cover}`}></div>
                     }
                     
                     {content.cover === "u-invisible" &&
-                        <div className={`laserbox__content-section ${content.page1}`}>
-                            <h2 className="laserbox__paragraph--italic">{zombiecatTexts.pitchP1[language]}</h2>
-                            <br></br>
-                            <p className="laserbox__paragraph--italic">{zombiecatTexts.pitchP2[language]}</p>
-                            <br></br>
-                            <p className="laserbox__paragraph--italic">{zombiecatTexts.pitchP3[language]}</p>
-                            <br></br>
-                            <h1 className="laserbox__paragraph--italic">{zombiecatTexts.pitchP4[language]}</h1>
-                            <br></br>
-                            <h2 className="laserbox__paragraph--italic u-margin-bottom-big">{zombiecatTexts.pitchP5[language]}</h2>
-                            <br></br>
-                            <p className="laserbox__paragraph">{zombiecatTexts.logoOrigin[language]}</p>
-                            <br></br>
-                            <p className="laserbox__paragraph--italic">{zombiecatTexts.logoPrompt[language]}</p>
-                        </div>
+                        <>
+                            {!easterEgg &&
+                                <div className={`laserbox__content-section ${content.page1}`}>
+                                    <h1 className="laserbox__paragraph--italic laserbox__paragraph--huge laserbox__paragraph--shiny">{zombiecatTexts.name[language]}</h1>
+                                    <br></br>
+                                    <p className="laserbox__paragraph">
+                                        <span className="laserbox__paragraph__chunk">{zombiecatTexts.explanationP1.firstHalf[language]}</span>
+                                        <span className="laserbox__paragraph__chunk laserbox__paragraph--italic laserbox__paragraph--huge laserbox__paragraph--shiny">{zombiecatTexts.explanationP1.secondHalf[language]}</span>
+                                    </p>
+                                    <br></br>
+                                    <p className="laserbox__paragraph">{zombiecatTexts.explanationP2[language]}</p>
+                                    <br></br>
+                                    <p className="laserbox__paragraph">{zombiecatTexts.explanationP3[language]}</p>
+                                    <br></br>
+                                    <p className="laserbox__paragraph">{zombiecatTexts.explanationP4[language]}</p>
+                                    <br></br>
+                                    <p className="laserbox__link u-margin-bottom-big" onClick={() => handleEasterEgg()}>
+                                        <span className="laserbox__paragraph__chunk">{zombiecatTexts.explanationP5[language]}</span>
+                                    </p>
+                                    <br></br>
+                                    <p className="laserbox__paragraph">{zombiecatTexts.logoOrigin[language]}</p>
+                                    <br></br>
+                                    <p className="laserbox__paragraph--italic">{zombiecatTexts.logoPrompt[language]}</p>
+                                </div>
+                            }
+
+                            {easterEgg &&
+                                <div className={`laserbox__content-section laserbox__content-section--cat-stars ${content.page2}`}>
+                                    <p className="laserbox__link" onClick={() => handleEasterEgg()}>
+                                        <span>{"\u2190"} {zombiecatTexts.backBtn[language]}</span>
+                                    </p>
+                                    <a href="https://www.instagram.com/msudrawings/" target="_blank" rel="noreferrer" class="laserbox__mention">
+                                        <p className="laserbox__paragraph--italic">{zombiecatTexts.credits[language]} Marc Solé Giménez</p>
+                                        <div className="u-flex">
+                                            <img class="laserbox__logoItem laserbox__logoItem--tiny" src={instagramLogo} alt="Instagram"/>
+                                            <p class="u-margin-left-tiny">@msudrawings</p>
+                                        </div>
+                                    </a>
+                                </div>
+                            }
+                        </>
                     }
                 </div>
             </LaserBox>
--- src/texts/ZombieCat_texts.js
+++ src/texts/ZombieCat_texts.js
@@ -1,28 +1,40 @@
 const zombiecatTexts = {
-    pitchP1: {
-        cat: "Desenvolupador Full Stack",
-        esp: "Desarrollador Full Stack",
-        eng: '"Adventurous and creative at heart, meticulous at what I do.'
+    name: {
+        cat: "Zombie",
+        esp: "Zombie",
+        eng: "Zombie"
     },
-    pitchP2: {
-        cat: "Desenvolupador Full Stack",
-        esp: "Desarrollador Full Stack",
-        eng: "After having tried many a professional sector and failed at some entrepreneurial ventures, I finally turned to writing code."
+    explanationP1: {
+        firstHalf: {
+            cat: "Aquest era el nom del meu ",
+            esp: "Este era el nombre de mi ",
+            eng: "This was the name of my "
+        },
+        secondHalf: {
+            cat: "Gat.",
+            esp: "Gato.",
+            eng: "Cat."
+        }
     },
-    pitchP3: {
-        cat: "Desenvolupador Full Stack",
+    explanationP2: {
+        cat: "Sempre vaig pensar que sonava guai. Una mica obscur, una mica, però sempre un toc entranyable.",
         esp: "Desarrollador Full Stack",
-        eng: "Loving the innovative always forward-looking ethos of the coding business, I'm always open to adapt and eager to learn and experiment with new technologies."
+        eng: "I always thought it sounded cool. Somewhat dark, somewhat spooky, but always bearing an endearing ring to it. When I started coding I chose it as the nickname for my GitHub account."
     },
-    pitchP4: {
-        cat: "Desenvolupador Full Stack",
-        esp: "Desarrollador Full Stack",
-        eng: "My purpose is to build friendly good-looking software solutions that help others get their jobs done."
+    explanationP3: {
+        cat: "Tristament, el Zombie de carn i ossos va morir a finals del 2022. Va ser una criatura noble, una ànima bonica i resplendent, tot un personatge, el meu amic i el meu company lleial.",
+        esp: "Tristemente, el Zombie de carne y huesos falleció a finales del 2022. Fue una criatura noble, un alma bonita y resplandeciente, todo un personaje, mi amigo y compañero leal.",
+        eng: "Sadly, flesh and bone Zombie passed away at the end of 2022. He was a noble creature, a beautiful and resplendent soul, a character outright, my friend and my loyal companion."
     },
-    pitchP5: {
-        cat: "Desenvolupador Full Stack",
-        esp: "Desarrollador Full Stack",
-        eng: 'Reliability. Creativity. Flexibility."'
+    explanationP4: {
+        cat: "Fer servir el seu nom i la seva imatge per construir la meva marca professional és una manera d'honorar la seva memòria i recordar totes les coses bones que va portar a la meva vida.",
+        esp: "Usar su nombre e imagen para construir mi marca profesional es una manera de honrar su memoria y recordar todas las cosas buenas que trajo a mi vida.",
+        eng: "Using his name and image to build my professional trademark is a way to honour his memory and recall all the good things he brought into my life."
+    },
+    explanationP5: {
+        cat: "A volar, petitó!",
+        esp: "¡A volar, pequeñín!",
+        eng: "Godspeed, little one!"
     },
     logoOrigin: {
         cat: "Desenvolupador Full Stack",
@@ -33,6 +45,16 @@ const zombiecatTexts = {
         cat: "Desenvolupador Full Stack",
         esp: "Desarrollador Full Stack",
         eng: '"A round logo featuring a cartoon-like brown furry siamese zombie blue-eyed cat."'
+    },
+    backBtn: {
+        cat: "Tornar",
+        esp: "Volver",
+        eng: "Go back"
+    },
+    credits: {
+        cat: "Autor:",
+        esp: "Autor:",
+        eng: "Author:"
     }
 }
 
